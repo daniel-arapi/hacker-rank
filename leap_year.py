52f3812e3a2d8e94f89c45c1b608ce6d37bb29c7
Daniel Arapi <darapi326@gmail.com>
2024-07-29 11:38:32 -0500

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -36,16 +36,9 @@ Explanation 0
 1990 is not a multiple of 4 hence it's not a leap year.
 """
 
-
-
 def is_leap(year):
-    leap = False
-    
-    if year % 4 == 0:
-        if (year % 100 == 0) and (year % 400 == 0):
-            leap = True
-        
-    return leap
+    return (year % 4 == 0) and (year % 100 != 0 or year % 400 == 0)
+
 
 year = int(input())
 print(is_leap(year))
